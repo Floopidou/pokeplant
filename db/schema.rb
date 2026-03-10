3a19edfadebe45d4253a1e3c5472d616bf2fa7dd
Perrine Villers <26814178+Floopidou@users.noreply.github.com>
2026-03-10 11:19:02 +0100

    user migration: added birthdate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_09_163446) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_10_101736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -83,6 +83,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_09_163446) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.date "birthdate"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
