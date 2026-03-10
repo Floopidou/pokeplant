867af92c74a0c05132f19e1fa1d5864ff3f99a89
Mickael Tremauville <mickael.tremauville@gmail.com>
2026-03-10 14:29:31 +0100

    create login/signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_writer :login
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                       format: { with: /\A[a-zA-Z0-9_]+\z/, message: "ne peut contenir que des lettres, chiffres et _" },
+                       length: { minimum: 3, maximum: 20 }
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_hash).where(
+        ["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]
+      ).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
 end
